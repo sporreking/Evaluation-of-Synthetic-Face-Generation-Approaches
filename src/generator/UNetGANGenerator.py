3a6f94c2dabac92d0763eaecd38c2f15a4ca5442
Ted Gustafsson <ted-gustafsson@hotmail.com>
2022-03-17 18:15:47 +0100

    feat: UNetGANGenerator class

---
+++ src/generator/UNetGANGenerator.py
@@ -0,0 +1,55 @@
+from src.generator.Generator import Generator
+import numpy as np
+from src.environment.EnvironmentManager import EnvironmentManager as em
+
+
+class UNetGANGenerator(Generator):
+    _DIM_Z = 128
+    _PATH_TO_IMAGES = "environment/unetgan/out/sample/"
+    _IMAGE_EXT = ".png"
+
+    def __init__(self):
+        super().__init__("U-NetGAN")
+
+    def latent_space_std(self) -> np.ndarray:
+        """
+        Return the standard deviation of each dimension in the latent space.
+
+        Returns:
+            np.ndarray: The standard deviations of the latent space distribution.
+        """
+        return np.ones(self._DIM_Z)
+
+    def latent_space_mean(self) -> np.ndarray:
+        """
+        Return the mean of each dimension in the latent space.
+
+        Returns:
+            np.ndarray: The means of the latent space distribution.
+        """
+        return np.zeros(self._DIM_Z)
+
+    def generate(self, latent_codes: np.ndarray) -> list[str]:
+        """
+        Generate images for the specified latent codes. This will most likely
+        involve the launch of a separate agent process using the EnvironmentManager,
+        allowing external generators to be launched in appropriate conda environments.
+
+        Args:
+            latent_codes (np.ndarray): The latent codes to generate images for.
+
+        Returns:
+            list[str]: A list of URIs to the generate images, in the same order as the
+                specified latent code input.
+        """
+        # Send codes via socket
+        em.send_latent_codes(latent_codes)
+
+        # Run U-NetGAN
+        em.run("unetgan")
+
+        # Return list of URIs
+        return [
+            self._PATH_TO_IMAGES + str(i) + self._IMAGE_EXT
+            for i in range(latent_codes.shape[0])
+        ]
