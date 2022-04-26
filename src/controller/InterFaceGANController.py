2bf227911ae9699afed3074f06f6b4afeefbb052
Ted Gustafsson <ted-gustafsson@hotmail.com>
2022-04-26 10:12:25 +0200

    feat: interfacegan controller
    also:
    - support vector classifier utility file
    - function for generating random standardized input in controller class
    - return manipulated latent codes from generate_native in controllers

---
+++ src/controller/InterFaceGANController.py
@@ -0,0 +1,349 @@
+# Inspired by https://github.com/genforce/interfacegan
+
+from src.controller.Controller import Controller
+from src.generator.Generator import Generator
+import numpy as np
+from src.controller.SupportVectorClassifier import (
+    get_svc_names,
+    get_missing_model_names,
+    setup_population,
+    setup_auxillary,
+    setup_labels,
+    train_svc,
+    get_svc,
+    is_population_ready,
+    get_population_name,
+    get_population_info,
+    is_labels_ready,
+)
+from src.controller.IdentityController import IdentityController
+from src.controller.ASADController import ASADController
+from typing import List, Tuple
+from sklearn.svm import SVC
+from src.core.Setupable import SetupMode
+from src.util.AuxUtil import load_aux_best
+
+IFGAN_NAME = "InterFaceGAN"
+STEP_MAX = 3  # * 5 max before bad results in paper
+USE_PROJECTED = True
+
+# Setup name constants
+SETUP_POPULATION_NAME = "SETUP_POPULATION"
+SETUP_LABELS_NAME = lambda attr: f"SETUP_LABELS_{attr}"
+SETUP_SVC_PREFIX = "TRAIN_SVC"
+
+
+class InterFaceGANController(Controller):
+    """
+    Subclass to Controller, implements InterFaceGAN.
+    """
+
+    def __init__(self, gen: Generator, attrs: list[str]):
+        """
+        Constructor to the InterFaceGAN
+
+        Args:
+            gen (Generator): The generator associated with the controller.
+            attrs (list[str]): Attributes to be used.
+        """
+        super().__init__(IFGAN_NAME, gen, attrs)
+
+        # For saving support vector classifiers
+        self._svcs = {}
+
+    def _setup_info_func(self, model_name: str) -> str:
+        info = load_aux_best(model_name)
+        return (
+            f"Best model was trained for {info.epoch + info.batch / info.num_batches_per_epoch:.3f} epochs | "
+            + f"Training loss = {info.train_loss:.6f} | Validation loss = {info.valid_loss:.6f}"
+        )
+
+    def reg_setup_modes(self) -> dict[str, SetupMode]:
+        # To get classifier models
+        asad_controller = ASADController(self._gen, self._attrs)
+
+        # Controller associated with generating
+        # the population.
+        controller = IdentityController(self._gen)
+
+        # QoL
+        mn_cls = lambda attr, omit_name=True: asad_controller.get_model_name(
+            attr, True, omit_name
+        )
+        gen_name = self._gen.get_name()
+
+        # TODO: Implement 'improve' functionality
+
+        # Construct modes
+        return {
+            SETUP_POPULATION_NAME: SetupMode(
+                lambda _: self._setup_pop(),
+                lambda pop_name=get_population_name(
+                    gen_name, controller.get_name()
+                ): is_population_ready(pop_name),
+                lambda pop_name=get_population_name(
+                    gen_name, controller.get_name()
+                ): get_population_info(pop_name),
+            ),
+            **{
+                mn_cls(attr): SetupMode(
+                    lambda _, batch_size, epochs, attr=attr: self._setup_auxillary_classifier(
+                        attr, batch_size, epochs
+                    ),
+                    lambda attr=attr: load_aux_best(mn_cls(attr, False)) is not None,
+                    lambda attr=attr: self._setup_info_func(mn_cls(attr, False)),
+                    [SETUP_POPULATION_NAME],
+                    batch_size=64,
+                    epochs=40,
+                )
+                for attr in self._attrs
+            },
+            **{
+                SETUP_LABELS_NAME(attr): SetupMode(
+                    lambda _, batch_size, epochs, attr=attr: self._setup_training_labels(
+                        attr, batch_size, epochs
+                    ),
+                    lambda attr=attr, pop_name=get_population_name(
+                        gen_name, controller.get_name()
+                    ): is_labels_ready(attr, pop_name),
+                    required_modes=[mn_cls(attr)],
+                    batch_size=64,
+                    epochs=40,
+                )
+                for attr in self._attrs
+            },
+            **{
+                (SETUP_SVC_PREFIX + "_" + attr): SetupMode(
+                    lambda _, attr=attr: self._train_svc(attr),
+                    lambda attr=attr: get_svc(attr, gen_name, controller.get_name())
+                    is not None,
+                    required_modes=[SETUP_LABELS_NAME(attr)],
+                )
+                for attr in self._attrs
+            },
+        }
+
+    def _setup_pop(self) -> None:
+        """
+        Setup a population, population used for training SVCs.
+        Setup already existing population or if necessary constructs a new one
+        and populate it.
+        """
+        setup_population(IdentityController(self._gen))
+
+    def _setup_auxillary_classifier(
+        self, attr: str, batch_size: int = 64, epochs: int = 40
+    ) -> None:
+        """
+        Train auxiliary classifier (borrowed from ASADController) if
+        necessary.
+
+        Trained classifiers are saved according to ASADController.
+        """
+        setup_auxillary(attr, self.get_generator().get_dataset(), batch_size, epochs)
+
+    def _setup_training_labels(
+        self, attr: str, batch_size: int = 64, epochs: int = 40
+    ) -> None:
+        """
+        Setup labels given a auxiliary classifier (Will train one if missing.)
+
+        See documentation for `_setup_auxillary_classifier` and the ASADController
+        class for more information about the classifier.
+        """
+        controller = IdentityController(
+            self.get_generator()
+        )  # Dummy controller, see setup_pop docs
+
+        setup_labels(controller, attr, batch_size, epochs)
+
+    def _train_svc(self, attr: str) -> None:
+        """
+        Train a support vector classifier to predict attributes on a population.
+        """
+        controller = IdentityController(
+            self._gen
+        )  # Dummy controller, see setup_labels docs
+
+        svc = train_svc(controller, attr)
+
+        # Save/Update model
+        self._svcs[attr] = svc
+
+    def parse_native_input(self, input: dict[str, np.ndarray]) -> np.ndarray:
+        """
+        Parse input from standardized representation to native representation.
+
+        Standardized representation refers to the float range of [-1,1].
+
+        For InterFaceGAN, the representation does not coincide with the float range of the native
+        representation. Thus, values are transformed from [-1,1] to [`STEP_MAX`,-`STEP_MAX`].
+
+
+        For example:
+
+        Let input be defined as:
+        `input_example = {"gender" : np.ndarray[-1, 1, -0.5], "age": np.ndarray[1,0,-1]}`
+        then input for image 1 should then be parsed as an image with gender = -1 * `STEP_MAX`
+        and age = 1 * `STEP_MAX`. Thus, `input example` defines parameters for 3 images (length of the array).
+
+        Note that a value of 0 means that no changes for that parameter will be done for that image.
+
+        Args:
+            input (dict[str, np.ndarray]): Standardized input representation.
+                Where key = parameter name, and the value = array of values
+                (in float range of [-1,1]) of that parameter. Each value in the
+                array represents one generated image. For example, image 1 should
+                be based on value indexed 1 in the array for each parameter in input.keys().
+
+        Returns:
+            np.ndarray[dict[str, Any]]: 2D array containing dictionary with
+                parameter names and their native values. Axis 0 corresponds to the number
+                of images to generate/manipulate, while axis 1 denotes different sequential
+                manipulation steps that should be applied, however, in InterFaceGAN all
+                manipulation can be done in one step. Thus the length of axis 1 should be 1.
+                Each entry in the array corresponds to a single manipulation specified by a
+                dictionary, with parameters on native format.
+        """
+        attrs = list(input.keys())
+        vals = list(input.values())
+
+        nr_images = len(vals[0])
+        nr_attrs = len(attrs)
+
+        parsed_input = np.empty((nr_images, 1), object)
+        for i in range(nr_images):
+            parsed_input[i, 0] = {
+                attrs[j]: vals[j][i] * STEP_MAX for j in range(nr_attrs)
+            }
+
+        return parsed_input
+
+    def generate_native(
+        self,
+        latent_codes: np.ndarray,
+        native_input: np.ndarray,
+        use_projected: bool = USE_PROJECTED,
+    ) -> Tuple[List[str], np.ndarray]:
+        gen_name = self._gen.get_name()
+
+        projected_boundaries_dict = {}
+
+        # Go through each latent code
+        for i in range(latent_codes.shape[0]):
+            d = native_input[i, 0]
+            attrs = list(d.keys())
+
+            boundaries_shape = (len(attrs), latent_codes.shape[1])
+            boundaries = np.zeros(boundaries_shape)
+
+            # Go through each attribute
+            for j, attr in enumerate(attrs):
+                # Get svcs
+                if attr in self._svcs:
+                    svc = self._svcs[attr]
+                else:
+                    svc = get_svc(attr, gen_name)
+                    self._svcs[attr] = svc
+
+                # Save boundaries
+                boundary = self._get_boundary(svc)
+                boundaries[
+                    j,
+                ] = boundary
+
+                if not use_projected or boundaries_shape[0] == 1:
+                    # Use boundaries to manipulate latent codes
+                    latent_codes[i,] += (
+                        d[attr] * boundary
+                    )
+
+            # Use conditioned/projected boundaries to manipulate latent codes
+            if use_projected and boundaries_shape[0] != 1:
+                sorted_attrs = sorted(attrs)
+                attr_key = "_".join(sorted_attrs)
+
+                # Check if projection already been done before
+                if attr_key in projected_boundaries_dict:
+                    projected_boundaries = projected_boundaries_dict[attr_key]
+                else:
+                    # Project each boundary conditioned on all other boundaries
+                    projected_boundaries = np.zeros(boundaries_shape)
+                    for j in range(projected_boundaries.shape[0]):
+                        projected_boundaries[j] = self._project_boundary(
+                            boundaries[
+                                j,
+                            ],
+                            np.delete(boundaries, j, axis=0),
+                        )
+
+                # Go through each attribute
+                for j, attr in enumerate(attrs):
+                    latent_codes[i,] += (
+                        d[attr]
+                        * boundaries[
+                            j,
+                        ]
+                    )
+
+        # Generate images based on updated latent codes
+        return self._gen.generate(latent_codes), latent_codes
+
+    def _get_svc_names(self) -> list[str]:
+        return get_svc_names(self._attrs, self._gen.get_name())
+
+    def _missing_models(self) -> list[str]:
+        return get_missing_model_names(self._get_svc_names())
+
+    def _get_boundary(self, svc: SVC) -> np.ndarray:
+        """
+        Returns the normalized boundary of the SVC.
+        """
+        boundary_coefs = svc.coef_.flatten().astype(np.float32)
+        return boundary_coefs / np.linalg.norm(boundary_coefs)
+
+    def _project_boundary(self, boundary: np.ndarray, boundaries: np.ndarray):
+        """
+        Returns a normalized boundary conditioned for other `boundaries`, resulting in a
+        boundary which allows for change with the associated attribute without (in theory)
+        change to the other attributes associated with the other boundaries.
+
+        Both `boundary` and `boundaries` should be normalized.
+
+        Boundary should be a row vector of same length as the latent space, `boundaries` should be a
+        collection of row vectors (same length as `boundary`), one for each attribute to condition for.
+        """
+        A = np.vstack([boundaries, boundary]).T
+
+        # From Strang, G. Introduction to linear algebra.
+        def _qr_factorization(A):
+            m, n = A.shape
+            Q = np.zeros((m, n))
+            R = np.zeros((n, n))
+
+            for j in range(n):
+                v = A[:, j]
+
+                for i in range(j - 1):
+                    q = Q[:, i]
+                    R[i, j] = q.dot(v)
+                    v = v - R[i, j] * q
+
+                norm = np.linalg.norm(v)
+                Q[:, j] = v / norm
+                R[j, j] = norm
+            return Q, R
+
+        # gram schmidt by qr factorization
+        Q, _ = _qr_factorization(A)
+        boundary = Q[:, -1].T
+
+        # Return normalized boundary
+        return boundary / np.linalg.norm(boundary)
+
+    def _setup_info_func(self, model_name: str):
+        info = load_aux_best(model_name)
+        return (
+            f"Best model was trained for {info.epoch + info.batch / info.num_batches_per_epoch:.3f} epochs | "
+            + f"Training loss = {info.train_loss:.6f} | Validation loss = {info.valid_loss:.6f}"
+        )
---
+++ src/controller/SupportVectorClassifier.py
@@ -0,0 +1,426 @@
+from pathlib import Path
+import joblib
+from src.util.FileJar import FileJar
+from src.controller.IdentityController import IdentityController, IDENTITY_NAME
+from src.population.Population import Population
+from src.controller.Controller import Controller
+from src.controller.ASADController import ASADController
+from src.generator.Generator import Generator
+from src.generator.UNetGANGenerator import UNetGANGenerator
+from src.dataset.Dataset import Dataset
+from src.dataset.TorchImageDataset import TorchImageDataset
+import torchvision.transforms as T
+import numpy as np
+from sklearn.svm import SVC
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+import src.util.CudaUtil as CU
+import math
+from typing import Tuple, Union, Any
+import torch
+
+#! Setup function should be run in order:
+#! setup_population -> setup_auxillary -> setup_labels -> train_svc
+# File handling constants
+SVC_PREFIX = "SVC"
+SVC_DIR = Path() / "auxiliary"
+SVC_FILE_EXT = ".joblib"
+LABELS_EXT = ".npy"
+
+# Model constans
+MIN_TRAIN_SIZE = 500000  # 500k used in interfacegan paper, 200k in styleganpaper
+CLS_CONFIDENCE = 1 / 5  # CLS_CONFIDENCE * MIN_TRAIN_SIZE = nr training samples
+
+
+def _get_svc_name(
+    attr: str, gen_name: str, controller_name: str = IDENTITY_NAME
+) -> str:
+    return "_".join((SVC_PREFIX, controller_name, gen_name, attr))
+
+
+def get_svc_names(
+    attrs: list[str], gen_name: str, controller_name: str = IDENTITY_NAME
+) -> list[str]:
+    """
+    Returns the names of all support vector classifiers given the parameters.
+
+    Args:
+        attrs (list[str]): List of attributes.
+        gen_name (str): The name of the generator which produced the training samples.
+        controller_name (str, optional): The name of the controller which produced the training samples.
+            Use Default `IDENTITY_NAME` if controller didnt perform any manipulations to the samples.
+            Defaults to IDENTITY_NAME.
+
+    Returns:
+        list[str]: SVC model names.
+    """
+    return [_get_svc_name(attr, gen_name, controller_name) for attr in attrs]
+
+
+def get_attr_from_svc_name(name: str) -> str:
+    """
+    Returns the attribute associated with the given SVC name.
+
+    Args:
+        name (str): SVC model name.
+
+    Returns:
+        str: The attribute associated with the given SVC name.
+    """
+    return name.split("_")[-1]
+
+
+def get_missing_model_names(all_svc_names: list[str]) -> list[str]:
+    """
+    Returns the names of all the missing support vector classifiers.
+
+    Args:
+        all_svc_names (list[str]): Name of all SVC names.
+
+    Returns:
+        list[str]: Names of the missing SVCs.
+    """
+    file_jar = FileJar(SVC_DIR)
+
+    return [
+        n
+        for n in all_svc_names
+        if file_jar.get_file(n + SVC_FILE_EXT, joblib.load) is None
+    ]
+
+
+def get_svc(
+    attr: str, gen_name: str, controller_name: str = IDENTITY_NAME
+) -> Union[SVC, None]:
+    """
+    Returns the SVC associated with the given parameters.
+
+    Args:
+        attr (str): Attribute which SVC is a predictor for.
+        gen_name (str): Generator associated with the training of the SVC.
+        controller_name (str, optional): Controller associated with the training of the SVC.
+            Use Default `IDENTITY_NAME` if controller didnt perform any manipulations to the samples.
+            Defaults to IDENTITY_NAME.
+
+    Returns:
+        Union[SVC, None]: The model if it was found, otherwise None.
+    """
+    file_jar = FileJar(SVC_DIR)
+
+    return file_jar.get_file(
+        _get_svc_name(attr, gen_name, controller_name) + SVC_FILE_EXT, joblib.load
+    )
+
+
+def get_population_name(gen_name: str, controller_name: str = IDENTITY_NAME) -> str:
+    """
+    Returns the name of the population associated with the given Controller.
+
+    Args:
+        gen_name (str): Generator associated with the population.
+        controller_name (str, optional): Controller associated with the population.
+            Use Default `IDENTITY_NAME` if controller didnt perform any manipulations to the samples.
+            Defaults to IDENTITY_NAME.
+
+    Returns:
+        str: The name of the population.
+    """
+    return "_".join((SVC_PREFIX, controller_name, gen_name))
+
+
+def get_population_info(pop_name: str) -> str:
+    """
+    Returns information about the population. (Number of samples)
+
+    Args:
+        pop_name (str): Name of the population.
+
+    Returns:
+        str: The information about the population.
+    """
+    return f"Population has {Population(pop_name).num_samples()} samples."
+
+
+def is_population_ready(pop_name: str) -> bool:
+    """
+    Checks if the population is ready by comparing the
+    number of samples to `MIN_TRAIN_SIZE`.
+
+    Args:
+        pop_name (str): Name of the population.
+
+    Returns:
+        bool: True if ready.
+    """
+    return Population(pop_name).num_samples() >= MIN_TRAIN_SIZE
+
+
+def setup_population(controller: Controller) -> None:
+    """
+    Setup a population given by a Controller, population used for training
+    SVCs. Setup already existing population or if necessary constructs a new one.
+
+    If population is not populated, then `_populate_population()` will be called.
+
+    Args:
+        controller (Controller): Used for defining the name of the population.
+        use dummy IdentityController(with the correct Generator) if no manipulations are needed.
+    """
+    # Get name of the population
+    pop_name = get_population_name(
+        controller.get_generator().get_name(), controller.get_name()
+    )
+
+    # Check population, populate if necessary
+    if not is_population_ready(pop_name):
+        _populate_population(Population(pop_name), controller)
+
+
+def setup_auxillary(
+    attr: str, ds: Dataset, batch_size: int = 64, epochs: int = 40
+) -> None:
+    """
+    Train auxiliary classifier (borrowed from ASADController) if
+    necessary. Retraining not supported.
+
+    Trained classifier are saved according to ASADController.
+    Args:
+        attr (str): Attribute of the auxiliary classifier.
+        ds (Dataset): Training data used/to be used by the classifier.
+        batch_size (int,optional): Batch size of the training. Defaults to 64.
+        epochs (int,optional): Number of epochs. Defaults to 40.
+    """
+
+    # Train classifier if necessary
+    #! classifier borrowed from ASADController
+    # TODO: break away classifier from ASADController such that
+    # TODO: it can more easily be used by other components.
+    # Create dummy controller, using dummy generator
+    asad = ASADController(UNetGANGenerator(ds), [attr])
+    asad.get_classifier(attr, batch_size=batch_size, epochs=epochs)
+
+
+def setup_labels(
+    controller: Controller, attr: str, batch_size: int = 64, epochs: int = 40
+) -> None:
+    """
+    Setup `attr`-labels given a auxiliary classifier (Will train one if missing.)
+
+    Labels of the population (defined by the controller) are predicted by
+    the auxiliary classifier.
+
+    Labels are saved (via FileJar) to disk in the root directory of the population.
+    For example:
+
+    '.../population_name/`attr`.npy'
+
+    Args:
+        controller (Controller): Controller associated with the use of the
+            auxillary model.
+        attr (str): The attribute of the labels, coincide with the attribute of
+            the auxiliary classifier.
+        batch_size (int,optional): Batch size used for predicting labels with classifier.
+            (also used for training classifier if needed.) Defaults to 64.
+        epochs (int,optional): Number of epochs used to train the classifier if needed. Defaults to 40.
+    """
+    # Get classifier, train classifier if necessary
+    #! classifier borrowed from ASADController
+    # TODO: break away classifier from ASADController such that
+    # TODO: it can more easily be used by other components.
+    # Create dummy controller, using dummy generator
+    asad = ASADController(controller.get_generator(), [attr])
+    cls_model = asad.get_classifier(attr, batch_size=batch_size, epochs=epochs)
+
+    # Init population
+    pop = Population(
+        get_population_name(
+            controller.get_generator().get_name(), controller.get_name()
+        )
+    )
+    n = pop.num_samples()
+
+    # Loads classifier to GPU
+    device = CU.get_default_device()
+    cls_model = CU.to_device(cls_model, device)
+
+    # Create torch image dataset from population
+    stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+    pop_data = pop.get_data()
+    pop_ds = TorchImageDataset(
+        pop_data["uri"].values.tolist(), T.Compose([T.ToTensor(), T.Normalize(*stats)])
+    )
+
+    # Create a dataloader
+    pop_dl = DataLoader(pop_ds, batch_size, shuffle=False, pin_memory=True)
+
+    # Predict labels
+    all_labels = []
+    with torch.no_grad():
+        for images in tqdm(
+            pop_dl, total=math.ceil(n / batch_size), desc="Classifying images..."
+        ):
+            # Send data to GPU
+            images = CU.to_device(images, device)
+
+            # Predict labels
+            labels = cls_model(images)
+
+            # Save labels
+            all_labels.append(labels.cpu().numpy())
+
+    # Concatenate all labels
+    labels = np.concatenate(all_labels)
+
+    # Save labels
+    file_jar = FileJar(Population.POPULATION_ROOT_DIR / pop.get_name())
+    file_jar.store_file(attr + LABELS_EXT, lambda p: np.save(p, labels))
+
+
+def is_labels_ready(attr: str, pop_name: str) -> bool:
+    """
+    Checks if labels are ready.
+
+    Args:
+        attr (str): Attribute to check labels for.
+        pop_name (str): Population to check labels for.
+
+    Returns:
+        bool: True if ready.
+    """
+    # Get labels
+    file_jar = FileJar(Population.POPULATION_ROOT_DIR / pop_name)
+    return file_jar.get_file(attr + LABELS_EXT, np.load) is not None
+
+
+def train_svc(controller: Controller, attr: str) -> SVC:
+    """
+    Train a support vector classifier to predict `attr` on the population defined by the
+    controller.
+
+    Args:
+        controller (Controller): Controller associated with the training data.
+        attr (str): The attribute of the labels, coincide with the attribute of
+            the auxiliary classifier.
+    """
+    # Init population
+    pop = Population(
+        get_population_name(
+            controller.get_generator().get_name(), controller.get_name()
+        )
+    )
+
+    # Get latent codes
+    pop_data = pop.get_data()
+    latent_codes = np.stack(pop_data["latent_code"].to_numpy())
+
+    # Get labels
+    file_jar = FileJar(Population.POPULATION_ROOT_DIR / pop.get_name())
+    labels = file_jar.get_file(attr + LABELS_EXT, np.load)
+
+    # Fit svc to data
+    return _fit_svc(
+        latent_codes,
+        labels,
+        get_svc_names(
+            [attr], controller.get_generator().get_name(), controller.get_name()
+        )[0],
+    )
+
+
+def _fit_svc(latent_codes: np.ndarray, labels: np.ndarray, name: str) -> SVC:
+    # Sanity check
+    if latent_codes.shape[0] != labels.shape[0]:
+        raise ValueError(
+            f"The number of latent codes ({latent_codes.shape[0]}) must match "
+            + f"the number of labels ({labels.shape[0]})."
+        )
+
+    # fit svc
+    # Codes/labels filtering according to InterFaceGAN/Linear separability
+    latent_codes, labels = _remove_uncertain_labels(latent_codes, labels)
+
+    svc = SVC(kernel="linear")
+    print("Training SVC...")
+    svc.fit(latent_codes, labels)
+    print("Training done!")
+
+    # Save to disk
+    file_jar = FileJar(SVC_DIR)
+    file_jar.store_file(name + SVC_FILE_EXT, lambda p: joblib.dump(svc, p))
+    return svc
+
+
+def _remove_uncertain_labels(
+    latent_codes: np.ndarray, labels: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray]:
+    """
+    return (shuffled) `latent_codes`, `labels` with the
+    number of samples multiplied by `CLS_CONFIDENCE`/2 highest/lowest confidence values.
+    """
+
+    # Sort by labels
+    ind = np.argsort(labels, axis=0).flatten()
+
+    # Get filter indices
+    n = labels.shape[0]
+    n_filter = math.floor(n * CLS_CONFIDENCE / 2)
+    filtered_inds = np.concatenate((ind[:n_filter], ind[(n - n_filter) :]))
+
+    # Filter latent codes
+    latent_codes = latent_codes[
+        filtered_inds,
+    ]
+
+    # Format labels
+    labels = np.concatenate(
+        [np.zeros(n_filter, dtype=np.int), np.ones(n_filter, dtype=np.int)], axis=0
+    )
+
+    # Shuffle data
+    assert latent_codes.shape[0] == labels.shape[0]
+    shuffle_inds = np.random.permutation(labels.shape[0])
+
+    return (
+        latent_codes[
+            shuffle_inds,
+        ],
+        labels[shuffle_inds],
+    )
+
+
+def _populate_population(pop: Population, controller: Controller) -> None:
+    """
+    Populate a population given the generator of the given controller.
+    """
+    # TODO support append to population
+    gen: Generator = controller.get_generator()
+
+    # Generate latent codes
+    latent_codes = gen.random_latent_code(MIN_TRAIN_SIZE)
+
+    # Generate native input
+    if isinstance(controller, IdentityController):
+        # No manipulation of latent codes
+        parsed_native_input = None
+        controller_input = {}
+    else:
+        # Uniform sample to get input
+        controller_input = controller.sample_random_input(
+            MIN_TRAIN_SIZE, attr_sample_mode="random"
+        )
+        parsed_native_input = controller.parse_native_input(controller_input)
+
+    # Generate samples, add samples to population
+    uris, manipulated_latent_codes = controller.generate_native(
+        latent_codes, parsed_native_input
+    )
+    #! Dummy values for columns not used
+    pop.add_all(
+        manipulated_latent_codes,
+        latent_codes,
+        uris,
+        [0 for i in range(0, MIN_TRAIN_SIZE)],  # TODO Filter support
+        append=False,
+        save_to_disk=True,
+        **controller_input,
+    )
