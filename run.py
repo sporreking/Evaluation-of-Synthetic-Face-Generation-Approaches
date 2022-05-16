2c8eb0d5ef8590eafe5cbb45801b95c251de3eb9
Alfred Sporre <alfred@familjensporre.se>
2022-05-16 13:00:39 +0200

    feat: setup phase support in run script
    note:
    * check if init phase is complete before running setup
    
    also:
    * ask user if they want to abort on attempts to re-run init phase

--- run.py
+++ run.py
@@ -1,26 +1,71 @@
 from argparse import ArgumentParser
+from typing import Callable
 import time
 import sys
 
-from src.init_phase import init_phase
+from src.init_phase import init_phase, init_done
+from src.setup_phase import setup_phase
+
+import src.util.PromptUtil as PU
+
+#####################
+# Utility functions #
+#####################
+
+
+def _require_phase(
+    target_phase_name: str, required_phase_name: str, check_func: Callable[[], bool]
+) -> None:
+    """Checks whether a required phase has been completed for a specified target phase."""
+    if not check_func():
+        print(
+            f"Could not run phase '{target_phase_name}' since the previous phase "
+            + f"'{required_phase_name}' has not yet been completed. Exiting."
+        )
+        exit(1)
+
+
+def _prompt_satisfied(phase_name: str, check_func: Callable[[], bool]) -> None:
+    """If the specified phase is already satisfied, the user will be asked if they want to re-run."""
+    if check_func():
+        if PU.prompt_yes_no(
+            f"Phase '{phase_name}' is already satisfied. Continue anyway?"
+        ):
+            print(f"Re-running phase '{phase_name}'.")
+        else:
+            print(f"Skipping phase '{phase_name}'. Exiting.")
+            exit(0)
+
 
 #################################################
 # Phase wrappers for specific argument handling #
 #################################################
 
 
-def _init_wrapper(name, argv=None):
+def _init_wrapper(name: str, argv: list = None) -> None:
+    # Ask user if phase should be re-run (if applicable)
+    _prompt_satisfied(name, init_done)
+
     # Execute init phase
     init_phase()
+
+
+def _setup_wrapper(name: str, argv: list = None) -> None:
+    # Require init phase
+    _require_phase(name, "init", init_done)
+
+    # Execute setup phase
+    setup_phase()
 
 
 ##########################
 # Core argument handling #
 ##########################
 
-# Process names
+# Phase names
 phases = {
     "init": _init_wrapper,
+    "setup": _setup_wrapper,
 }
 
 # Parser core arguments
