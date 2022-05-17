b9f07b0b06bc7b3fc2edf1099bdf359d8dadda07
Alfred Sporre <alfred@familjensporre.se>
2022-05-17 22:39:53 +0200

    feat: controller and CMM getters in compound model factory
    also:
    * remove borders from prints in compound model factory class

--- src/compound_model/CompoundModelFactory.py
+++ src/compound_model/CompoundModelFactory.py
@@ -370,11 +370,10 @@ class CompoundModelFactory:
         recalculate_metrics: bool = False,
         **parameters: Any,
     ) -> None:
-
-        msg = f"# Evaluating metrics for compound model: {self._compound_model_name_prefix}_{filter.get_name()} #"
-        print(len(msg) * "#")
-        print(msg)
-        print(len(msg) * "#")
+        print(
+            "# ~ Evaluating metrics for compound model: "
+            + f"{self._compound_model_name_prefix}_{filter.get_name()}"
+        )
 
         # Calc all
         if recalculate_metrics:
@@ -385,20 +384,14 @@ class CompoundModelFactory:
 
         # Calc missing
         else:
-            msg = f"# Trying to fetch metrics #"
-            print(len(msg) * "#")
-            print(msg)
-            print(len(msg) * "#")
+            print(f"# ~ Trying to fetch metrics")
             self._cmms[filter.get_name()].get(
                 compound_metrics,
                 calc_if_missing=True,
                 **parameters,
             )
 
-        msg = "# Metric Evaluation complete #"
-        print(len(msg) * "#")
-        print(msg)
-        print(len(msg) * "#")
+        print("# ~ Metric Evaluation complete")
 
     def get_filters(self) -> list[Type[Filter]]:
         """
@@ -435,6 +428,28 @@ class CompoundModelFactory:
             Population: The population.
         """
         return self._population
+
+    def get_controller(self) -> Controller:
+        """
+        Returns the controller of this compound model factory.
+
+        Returns:
+            Controller: The controller of this compound model factory.
+        """
+        return self._controller
+
+    def get_compound_metric_managers(self) -> list[CompoundMetricManager]:
+        """
+        Returns the compound metric managers of this compound model factory.
+
+        There should be precisely one compound metric manager for each filter
+        supplied on construction, i.e.,
+        `len(get_filters()) == len(get_compound_metric_managers())`.
+
+        Returns:
+            list[CompoundMetricManager]: The compound metric managers.
+        """
+        return [cmm for cmm in self._cmms.values()]
 
     def get_compound_model_names(self) -> list[str]:
         """
