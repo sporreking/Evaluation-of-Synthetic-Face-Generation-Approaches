3ee84a3a446b57597a125fdfcfa70c69fce18e3e
Alfred Sporre <alfred@familjensporre.se>
2022-05-25 11:43:26 +0200

    enhance: use W space for image generation in styleswin
    note:
    * add preprocessing step for projecting Z to W
    * solution similar to stylegan2-ada

--- src/generator/StyleSwinGenerator.py
+++ src/generator/StyleSwinGenerator.py
@@ -14,6 +14,7 @@ class StyleSwinGenerator(Generator):
     _DIM_Z = 512
     _PATH_TO_IMAGES = "environment/styleswin/out/"
     _IMAGE_EXT = ".png"
+    _W_FILE = "w.npy"
 
     def __init__(self, dataset: Dataset):
         """
@@ -30,17 +31,51 @@ class StyleSwinGenerator(Generator):
     def latent_space_mean(self) -> np.ndarray:
         return np.zeros(self._DIM_Z)
 
+    def _launch_generator(self, latent_codes: np.ndarray, mode: str = "generate"):
+
+        # Sanity check
+        assert mode in ("project", "generate")
+
+        # Send codes via socket
+        EM.send_latent_codes(latent_codes)
+
+        # Run StyleSwin
+        if not EM.run(self.get_name(), launch_mode=mode):
+            raise RuntimeError("Agent failed! Could not generate images.")
+
     def preprocess_latent_code(self, latent_codes: np.ndarray) -> np.ndarray:
         """
-        No preprocessing is needed. Returns the input.
+        Performs preprocessing of `latent_codes` such that they may be used by
+        the `generate()` method.
+
+        For StyleSwin, every latent code in `latent_codes` is expected to be in the
+        Z-space, and the output of this method will be their projection onto the W-domain.
 
         Args:
-            latent_codes (np.ndarray): Forwarded to the return.
+            latent_codes (np.ndarray): The latent codes to project, specified
+                on a per-row basis. The values should be from the Z-domain, preferably
+                drawn from the distribution described by `latent_space_std()` and
+                `latent_space_mean()`.
 
         Returns:
-            np.ndarray: Forwarded input `latent_codes`.
+            np.ndarray: The projection of the latent codes onto W, specified on a per-row basis.
+
+        Raises:
+            FileNotFoundError: If the Z -> W projection output file was not found.
         """
-        return latent_codes
+
+        # Project latent codes
+        self._launch_generator(latent_codes, "project")
+
+        # Check if successful
+        projection_output_file = self._PATH_TO_IMAGES + self._W_FILE
+        if not path.isfile(projection_output_file):
+            raise FileNotFoundError(
+                f"Could not find projection output file: '{projection_output_file}'"
+            )
+
+        # Load projection
+        return np.load(projection_output_file)
 
     def generate(self, latent_codes: np.ndarray) -> List[str]:
         """
@@ -48,9 +83,14 @@ class StyleSwinGenerator(Generator):
         of a separate agent process using the EnvironmentManager, allowing an external
         generator to be launched in its appropriate conda environments.
 
+        Every latent code in `latent_codes` is expected to be in the W-domain.
+
         Args:
-            latent_codes (np.ndarray): The latent codes to generate images for.
-                No preprocessing is required for the latent codes.
+            latent_codes (np.ndarray): The latent codes to generate images for. Make
+                sure that the codes have been properly preprocessed with
+                `preprocess_latent_code()` method, such that they are in W-space. Note
+                that `random_latent_code()` performs preprocessing automatically, i.e.,
+                the output from that method MUST NOT be processed again.
 
         Returns:
             list[str]: A list of URIs to the generate images, in the same order as the
@@ -60,12 +100,8 @@ class StyleSwinGenerator(Generator):
             FileNotFoundError: If any of the expected image output files were not found.
         """
 
-        # Send codes via socket
-        EM.send_latent_codes(latent_codes)
-
-        # Run StyleSwin
-        if not EM.run(self.get_name()):
-            raise RuntimeError("Agent failed! Could not generate images.")
+        # Generate images from latent codes
+        self._launch_generator(latent_codes)
 
         # Construct list of URIs
         uris = [
